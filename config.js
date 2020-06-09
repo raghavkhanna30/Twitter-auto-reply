4025b6123a0519827791931cf7039a02f89e7c43
Raghav khanna <48377506+raghavkhanna30@users.noreply.github.com>
2020-06-09 20:15:28 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  subdomain: "api",
+  consumer_key: "ENTER YOUR KEY HERE",
+  consumer_secret: "ENTER YOUR KEY HERE",
+  access_token_key: "ENTER YOUR KEY HERE",
+  access_token_secret: "ENTER YOUR KEY HERE"
+};
